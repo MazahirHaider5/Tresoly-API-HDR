70573df88f372a35d3b101c835a687b769d1e32e
CodeWithHaiderr <mazahirhaider98@gmail.com>
2025-02-05 16:36:22 +0500

    recently used APIand categories API for dashboard

--- src/routes/vault.routes.ts
+++ src/routes/vault.routes.ts
@@ -1,6 +1,6 @@
-    import { Router } from "express";
+import { Router } from "express";
 import { verifyToken } from "../middlewares/authenticate";
-import { createVault, deleteVault, getAllVaults, getUserVaults, updateVault, getVaultCategoryCounts } from "../controllers/vault.controller";
+import { createVault, deleteVault, getAllVaults, getUserVaults, updateVault, getVaultCategoryCounts, getRecentlyUsedVaults } from "../controllers/vault.controller";
 
 
 const router = Router();
@@ -12,6 +12,7 @@ router.delete("/deleteVault/:vaultId", verifyToken, deleteVault);
 
 router.get("/getAllVaults", getAllVaults);
 router.get('/categoryCount', getVaultCategoryCounts);
+router.get('/recentlyUsed', getRecentlyUsedVaults);
 
 
 export default router;
